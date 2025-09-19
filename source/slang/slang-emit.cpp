b302232511a9afbd001cb0eb67eeb1987b6aa01e
Yong He <yonghe@outlook.com>
2025-09-19 03:53:26 -0700

    Add test case, enhance transform-params-to-constref.

--- source/slang/slang-emit.cpp
+++ source/slang/slang-emit.cpp
@@ -1708,6 +1708,7 @@ Result linkAndOptimizeIR(
         if (targetProgram->getOptionSet().getBoolOption(
                 CompilerOptionName::EnableExperimentalPasses))
             introduceExplicitGlobalContext(irModule, target);
+        transformParamsToConstRef(irModule, codeGenContext->getSink());
 #if 0
         dumpIRIfEnabled(codeGenContext, irModule, "EXPLICIT GLOBAL CONTEXT INTRODUCED");
 #endif
